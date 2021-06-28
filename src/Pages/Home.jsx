0cd763e6036a304c588f20d8a4395bc679d1f2a8
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-27 22:01:33 -0300

    add check out

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { search } from "../Api/api";
+import { useHistory } from "react-router-dom";
+import { api, search } from "../Api/api";
 
 const Item = styled.div`
   box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
@@ -38,10 +39,12 @@ const Btn = styled.a`
   border: 2px solid white;
   background: #a7b307;
   color: white;
+  cursor: pointer;
 `;
 
 function Home() {
   const [data, setData] = useState([]);
+  let history = useHistory();
 
   useEffect(() => {
     search("/register", setData);
@@ -54,13 +57,27 @@ function Home() {
       <Btn href="/checkIn">check in</Btn>
 
       {data.map((item) => {
-        console.log(item);
+        const handleClick = async () => {
+          api
+            .delete(`/register/${item.alienId}`)
+            .then((response) => {
+              history.go(0);
+            })
+            .catch((error) => {
+              if (error.response) {
+                alert(error.response.data.error);
+              }
+              console.log(error.config);
+            });
+        };
+
         return (
           <Item key={item.registerId}>
             <Text>
               <span>Id do alien: {item.alienId}</span>
               <span>Id da balada: {item.partyId}</span>
               <span>checkIn: {item.checkIn}</span>
+              <Btn onClick={handleClick}>check out</Btn>
             </Text>
           </Item>
         );
